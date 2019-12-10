b352e5e30b3daf59480942264331e3df0397eb56
Duc Trinh <dtrinh914@gmail.com>
2019-12-09 16:22:44 -0800

    added example email credential file

---
+++ email/MAIL_SERVER_example.js
@@ -0,0 +1,9 @@
+module.exports = {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false,
+    auth: {
+    user: 'yutfggtgifd7ixet@ethereal.email',
+    pass: 'tX29P4QNadD7kAG7x5'
+    }
+};
