a6a6eec4a2f847ea9a1eaa381f168d4161865241
Duc Trinh <dtrinh914@gmail.com>
2019-12-10 16:05:35 -0800

    added a function to check if all of a certain type of input is empty

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,12 +11,13 @@ function addPerson(){
                         <button class='deleteBtn'><i class=" fas fa-times"></i></button>
                         <input type="text" name='name' placeholder='Name'></td>
                         <td><input name='email' type='email' placeholder='Email'></td>`
+    //appends html to DOM and adds event listener for delete button
     document.querySelector('#participants tbody').appendChild(person)
     const deleteBtn = document.querySelector(`.person[data-key=${id}] .deleteBtn`);
     deleteBtn.addEventListener('click', handleDelete);
 }
 
-//calls addPerson()
+//creates 5 more participants by calling addPerson
 function handleAdd(evt){
     evt.preventDefault();
     for(let i = 0; i < 5; i++){
@@ -45,10 +46,42 @@ function handleSubmit(evt){
 
 //adding event handlers to btns
 addBtn.addEventListener('click', handleAdd);
-// submitBtn.addEventListener('click', handleSubmit);
+submitBtn.addEventListener('click', handleSubmit);
 
+
+//                   //
+// UTILITY FUNCTIONS //
+//                   //
 
 //function to generate unique ids 
 var ID = function () {
     return '_' + Math.random().toString(36).substr(2, 9);
   };
+//check to see if string is blank or only white-space
+function isStrEmpty(str){
+    if(str === undefined || str === null){
+        return true;
+    }
+    if (/\S/.test(str)){
+        return false;
+    }
+    return true;
+}
+
+//                          //
+//FORM VALIDATION FUNCTIONS //
+//                          // 
+
+//checks to see which items in a group of fields are empty
+//returns the data-key of the parent of those empty fields
+function areFieldsEmpty(field){
+    const selected = document.querySelectorAll(field);
+    const empty = [];
+    selected.forEach( item => {
+        if(isStrEmpty(item.value)){
+            let parentID = item.parentElement.parentElement.getAttribute('data-key');
+            empty.push(parentID);
+        }
+    });
+    return empty;
+}
